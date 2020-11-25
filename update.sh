e2f7ae4fb34b7342f51d3c185ee3798e9bf04bd8
lab532 <3016204248@tju.edu.cn>
2020-11-25 14:02:30 +0800

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 repo_dir=$(pwd)
 cd $repo_dir
-#python $repo_dir/getIpAddress.py
+git pull #python $repo_dir/getIpAddress.py
 ip=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
 echo $ip $(date) > $repo_dir/pyb.txt
 git add ./pyb.txt
