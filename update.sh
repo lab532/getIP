c989e115e41aef29d48bf4bdc72d1ba07b776303
lab532 <3016204248@tju.edu.cn>
2021-01-07 10:10:12 +0800

    delete 2,3

--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@ repo_dir=$(pwd)
 cd $repo_dir
 git pull #python $repo_dir/getIpAddress.py
 ip=$(ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:")
-echo $1 " min/hour update" $ip $(date) > $repo_dir/hiedean.txt
-git add ./hiedean.txt
+echo $ip $(date) > $repo_dir/1_hiedean.txt
+git add ./1_hiedean.txt
 # git stash -u -k
-git commit -m "from hiedean.txt"
+git commit -m "from 1_hiedean.txt"
 # git pull
 git push origin master
 # git stash pop
